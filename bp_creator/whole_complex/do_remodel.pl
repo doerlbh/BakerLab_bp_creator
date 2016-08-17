d9510b35af21222ee3fdca129c31a0ed0e85faa0
doerlbh <doerlbh@gmail.com>
2016-08-17 02:25:51 -0700

    update code

--- bp_creator/whole_complex/do_remodel.pl
+++
@@ -1,5 +0,0 @@
-for my $d (`ls -d *fragments`)
-{
-	chomp($d);
-	system("");
-}
--- bp_creator/whole_complex/run.sh
+++
@@ -1,7 +0,0 @@
-#153 is the first residue of the second chain
-#-rg 0, do not use radius of gyration for elongated structures
-#cst is a distance constraint file, see bp file and cst for formatting
-~possu/src/git/Rosetta/main/source/bin/remodel.static.linuxgccrelease -s joint_renum.pdb -remodel:blueprint test.bp -two_chain_tree 153 \
--num_trajectory 5 -save_top 5  -no_jumps -overwrite -rg 0 -out:prefix test1 -jd2:no_output -enzdes:cstfile cst \
--core_cutoff 20 -boundary_cutoff 40 -remodel:use_pose_relax 
-#-remodel:use_cart_relax -remodel:quick_and_dirty 
